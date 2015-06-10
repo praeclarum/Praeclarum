4dd8f8b8d9f23a318909b9dabc0e373b7223eb9d
Frank A. Krueger <fak@praeclarum.org>
2015-06-10 14:38:33 -0700

    Adding new NView controls lib

---
+++ Praeclarum.Controls.Mac/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Praeclarum.Controls.Mac
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ Praeclarum.Controls.iOS/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Praeclarum.Controls.iOS
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+

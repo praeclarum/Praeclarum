f76d92c4dd8c367403e17586fbecdfa8a1f69444
Frank A. Krueger <fak@praeclarum.org>
2022-12-31 15:17:16 -0800

    Show PForm sections on Mac

--- Praeclarum.Mac/UI/PFormViewController.cs
+++ Praeclarum.Mac/UI/PFormViewController.cs
@@ -8,17 +8,41 @@ using CoreGraphics;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Praeclarum.UI
 {
 	public partial class PForm : NSViewController, IThemeAware
 	{
+		readonly NSStackView sectionsStack = new NSStackView
+		{
+			Orientation = NSUserInterfaceLayoutOrientation.Vertical,
+			Alignment = NSLayoutAttribute.Leading,
+		};
+
+		readonly Dictionary<PFormSection, PFormSectionView> sectionViews = new();
+
 		public PForm(string title = "")
+			: base(nibNameOrNull: null, nibBundleOrNull: null)
 		{
 			Title = (title ?? "").Localize ();
 
 			sections = new ObservableCollection<PFormSection> ();
 			sections.CollectionChanged += HandleSectionsChanged;
+		}
+
+		public override void LoadView ()
+		{
+			View = sectionsStack;
+			ReloadAll();
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			//sectionsStack.Frame = this.View.Bounds;
+			//sectionsStack.TranslatesAutoresizingMaskIntoConstraints = true;
+			//this.View.AddSubview(sectionsStack);
 		}
 
 		public override void ViewWillAppear ()
@@ -45,18 +69,107 @@ namespace Praeclarum.UI
 
 		public void ReloadAll ()
 		{
+			var visibleSectionViews = new List<PFormSectionView> (sectionsStack.ArrangedSubviews.OfType<PFormSectionView> ());
+			visibleSectionViews.MergeInto(sections,
+				(s, d) => s.Section.Equals(d),
+				s => new PFormSectionView(s),
+				(s, d) => s.ReloadSection(),
+				d => { });
+			sectionsStack.SetViews(visibleSectionViews.ToArray(), NSStackViewGravity.Top);
 		}
 
 		public void ReloadSection (PFormSection section)
 		{
+			if (sectionViews.TryGetValue(section, out var sv))
+			{
+				sv.ReloadSection();
+			}
 		}
 
 		public void FormatSection (PFormSection section)
 		{
+			if (sectionViews.TryGetValue (section, out var sv))
+			{
+				sv.FormatSection ();
+			}
 		}
 
 		public void ApplyTheme (Theme theme)
 		{
+			foreach (var ssv in sectionViews)
+			{
+				ssv.Value.ApplyTheme(theme);
+			}
+		}
+
+		public static void ShowWindow(NSObject? sender, ref NSWindow? window, Func<PForm> createForm)
+		{
+			if (window is NSWindow w)
+			{
+				w.OrderFront(sender);
+			}
+			else
+			{
+				var form = createForm();
+				w = new PFormWindow(form);
+				window = w;
+				w.MakeKeyAndOrderFront(sender);
+			}
+		}
+
+		public override CGSize PreferredContentSize {
+			get => new CGSize(320.0, 480.0);
+			set { } }
+	}
+
+	class PFormSectionView : NSStackView, IThemeAware
+	{
+		readonly NSTextField titleLabel;
+		readonly NSTextField hintLabel;
+
+		public PFormSection Section { get; }
+
+		public PFormSectionView(PFormSection section)
+		{
+			Section = section;
+			Orientation = NSUserInterfaceLayoutOrientation.Vertical;
+			Alignment = NSLayoutAttribute.Leading;
+
+			titleLabel = NSTextField.CreateLabel (section.Title);
+			titleLabel.Font = NSFont.BoldSystemFontOfSize(NSFont.SystemFontSizeForControlSize(NSControlSize.Large));
+			hintLabel = NSTextField.CreateWrappingLabel (section.Hint);
+			hintLabel.Font = NSFont.SystemFontOfSize(NSFont.SystemFontSizeForControlSize(NSControlSize.Regular));
+
+			AddView(titleLabel, NSStackViewGravity.Top);
+			AddView(hintLabel, NSStackViewGravity.Top);
+			ReloadSection();
+		}
+
+		public void ReloadSection ()
+		{
+			titleLabel.StringValue = Section.Title;
+			hintLabel.StringValue = Section.Hint;
+		}
+
+		public void FormatSection ()
+		{
+
+		}
+
+		public void ApplyTheme (Theme theme)
+		{
+		}
+	}
+
+	class PFormWindow : NSWindow
+	{
+		private readonly PForm form;
+
+		public PFormWindow(PForm form)
+			: base(new CGRect(CGPoint.Empty, form.PreferredContentSize), NSWindowStyle.Titled | NSWindowStyle.Closable | NSWindowStyle.Resizable, NSBackingStore.Buffered, deferCreation: false)
+		{
+			this.form = form;
+			this.ContentViewController = form;
 		}
 	}
 }

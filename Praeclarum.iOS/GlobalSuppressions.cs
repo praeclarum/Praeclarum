66e727f680021afd96ecccdd26e242e6134c81a4
Frank A. Krueger <fak@praeclarum.org>
2025-05-14 14:38:45 -0700

    Warnings as errors

---
+++ Praeclarum.iOS/GlobalSuppressions.cs
@@ -0,0 +1,2 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ("Obsoleted APIs", "CA1416", Justification = "False positives")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage ("Obsoleted APIs", "CA1422", Justification = "False positives")]

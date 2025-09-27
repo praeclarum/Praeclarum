ccc955773b9e507050d0f3ef733da2afe22c0a82
Frank A. Krueger <fak@praeclarum.org>
2025-09-27 11:12:26 -0700

    Use Selected property on UIBarButtonItem

--- Praeclarum.iOS/UI/SelectableButtonItem.cs
+++ Praeclarum.iOS/UI/SelectableButtonItem.cs
@@ -1,3 +1,5 @@
+#nullable enable
+
 using System;
 using UIKit;
 
@@ -7,30 +9,47 @@ namespace Praeclarum.UI
 	{
 		public UIBarButtonItem Item { get; private set; }
 
-		readonly UIButton button;
+		readonly UIButton? button = null;
+
+		private bool selected;
 
 		public bool Selected {
 			get {
-				return button.Selected;
+				return selected;
 			}
-			set {
-				button.Selected = value;
+			set
+			{
+				if (selected == value) return;
+				selected = value;
+				if (button is { } b)
+				{
+					b.Selected = selected;
+				}
+				else
+				{
+					if (ios15)
+					{
+						Item.Selected = selected;
+					}
+				}
 			}
 		}
 
-		readonly static bool ios7 = UIDevice.CurrentDevice.CheckSystemVersion (7, 0);
+		readonly static bool ios15 = UIDevice.CurrentDevice.CheckSystemVersion (15, 0);
 
 		public SelectableButtonItem (UIImage image, EventHandler handler)
 		{
-			if (ios7) {
+			if (ios15) {
+				Item = new UIBarButtonItem (image, UIBarButtonItemStyle.Plain, handler);
+				Item.Selected = selected;
+			}
+			else {
 				button = UIButton.FromType (UIButtonType.RoundedRect);
 				button.SetImage (image, UIControlState.Normal);
 				button.Frame = new CoreGraphics.CGRect (0, 0, 44, 44);
 				button.TouchUpInside += handler;
+				button.Selected = selected;
 				Item = new UIBarButtonItem (button);
-			} else {
-				Item = new UIBarButtonItem (image, UIBarButtonItemStyle.Plain, handler);
-				button = UIButton.FromType (UIButtonType.Custom);
 			}
 		}
 	}
